7bd4a47d2e6ae94e64bf87e1392eee43f522b833
Andrii Fytsyk <phytsyk@gmail.com>
2019-07-05 10:20:06 +0300

    change interfaces

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var Header = { name: "Sections", checkAllViews: false, checkAllEdits: false, checkAllRemoves: false, disabled: true };
 var Categories = [
-    { id: 1, name: "Calendar", view: false, edit: false, remove: false },
-    { id: 2, name: "Profile", view: false, edit: false, remove: false },
-    { id: 3, name: "Property", view: false, edit: false, remove: false },
-    { id: 4, name: "Contacts", view: false, edit: false, remove: false }
+    { id: 1, name: "Calendar", view: { checked: false, disabled: false }, edit: { checked: false, disabled: true }, remove: { checked: false, disabled: true } },
+    { id: 2, name: "Profile", view: { checked: false, disabled: false }, edit: { checked: false, disabled: true }, remove: { checked: false, disabled: true } },
+    { id: 3, name: "Property", view: { checked: false, disabled: false }, edit: { checked: false, disabled: true }, remove: { checked: false, disabled: true } },
+    { id: 4, name: "Contacts", view: { checked: false, disabled: false }, edit: { checked: false, disabled: true }, remove: { checked: false, disabled: true } }
 ];
 var HerosComponentController = /** @class */ (function () {
     function HerosComponentController() {
@@ -18,7 +18,7 @@ var HerosComponent = /** @class */ (function () {
     function HerosComponent() {
         this.controller = HerosComponentController;
         this.controllerAs = "$ctrl";
-        this.template = "\n<div class=\"row d-flex justify-content-center\">\n    <table class=\"table col-6\">\n    <thead>\n        <tr>\n            <th scope=\"col\">{{$ctrl.header.name}}</th>\n            <th scope=\"col\"><input type=\"checkbox\" ng-model=\"$ctrl.header.checkAllViews\" disabled=\"$ctrl.header.disabled\">Check All</th>\n            <th scope=\"col\"><input type=\"checkbox\" ng-model=\"$ctrl.header.checkAllEdits\" disabled=\"$ctrl.header.disabled\">Check All</th>\n            <th scope=\"col\"><input type=\"checkbox\" ng-model=\"$ctrl.header.checkAllRemoves\" disabled=\"$ctrl.header.disabled\">Check All</th>\n        </tr>\n    </thead>\n    <tbody>\n        <tr ng-repeat=\"category in $ctrl.categories\" class=\"bottom-border-dotted\">\n            <td>{{category.name}}</td>\n            <td><input type=\"checkbox\" ng-model=\"category.view\">View</td>\n            <td><input type=\"checkbox\" ng-model=\"category.edit\">Edit</td>\n            <td><input type=\"checkbox\" ng-model=\"category.remove\">Remove</td>\n        </tr>\n    </tbody>\n</table>\n</div>\n<div class=\"row d-flex justify-content-center\">\n<div class=\"col-6\">\n<button type=\"button\" class=\"btn btn-primary ml-auto d-block\">Save</button>\n</div>\n    \n</div>\n\n\n    ";
+        this.templateUrl = "./content.html";
     }
     return HerosComponent;
 }());
--- script.ts
+++ script.ts
@@ -17,7 +17,7 @@ interface IHeader {
 const Header: IHeader = {name: "Sections", checkAllViews: false, checkAllEdits:false, checkAllRemoves:false, disabled: true};
 
 const Categories: ICategories[] = [
-    { id: 1, name: "Calendar", view: {checked: true, disabled: false}, edit: {checked: false, disabled: true}, remove:{checked: false, disabled: true} },
+    { id: 1, name: "Calendar", view: {checked: false, disabled: false}, edit: {checked: false, disabled: true}, remove:{checked: false, disabled: true} },
     { id: 2, name: "Profile", view: {checked: false, disabled: false}, edit: {checked: false, disabled: true}, remove:{checked: false, disabled: true} },
     { id: 3, name: "Property", view: {checked: false, disabled: false}, edit: {checked: false, disabled: true}, remove:{checked: false, disabled: true} },
     { id: 4, name: "Contacts", view: {checked: false, disabled: false}, edit: {checked: false, disabled: true}, remove:{checked: false, disabled: true} }
@@ -40,41 +40,12 @@ class HerosComponent implements ng.IComponentOptions {
 
     public controller: ng.Injectable<ng.IControllerConstructor>;
     public controllerAs: string;
-    public template: string;
+    public templateUrl: string;
 
     constructor() {
         this.controller = HerosComponentController;
         this.controllerAs = "$ctrl";
-        this.template = `
-<div class="row d-flex justify-content-center">
-    <table class="table col-6">
-    <thead>
-        <tr>
-            <th scope="col">{{$ctrl.header.name}}</th>
-            <th scope="col"><input type="checkbox" ng-model="$ctrl.header.checkAllViews" disabled="$ctrl.header.disabled">Check All</th>
-            <th scope="col"><input type="checkbox" ng-model="$ctrl.header.checkAllEdits" disabled="$ctrl.header.disabled">Check All</th>
-            <th scope="col"><input type="checkbox" ng-model="$ctrl.header.checkAllRemoves" disabled="$ctrl.header.disabled">Check All</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr ng-repeat="category in $ctrl.categories" class="bottom-border-dotted">
-            <td>{{category.name}}</td>
-            <td><input type="checkbox" ng-model="category.checked.view">View</td>
-            <td><input type="checkbox" ng-model="category.checked.edit">Edit</td>
-            <td><input type="checkbox" ng-model="category.checked.remove">Remove</td>
-        </tr>
-    </tbody>
-</table>
-</div>
-<div class="row d-flex justify-content-center">
-<div class="col-6">
-<button type="button" class="btn btn-primary ml-auto d-block">Save</button>
-</div>
-    
-</div>
-
-
-    `;
+        this.templateUrl = `./content.html`;
     }
 }
 
